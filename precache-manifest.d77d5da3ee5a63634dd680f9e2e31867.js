fb15ebf12d534bb6a92ba5b5a1ff396242b4a507
TLeonBrown <tianbrown@gmail.com>
2020-12-09 19:20:23 -0500

    Updates

--- precache-manifest.d77d5da3ee5a63634dd680f9e2e31867.js
+++ precache-manifest.d77d5da3ee5a63634dd680f9e2e31867.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9755804e20e0e303ac12da0213eae191",
+    "revision": "4275c4977faa1d51bd2d9bfb60c22ae9",
     "url": "/index.html"
   },
   {
-    "revision": "e967eb79f5813e467632",
+    "revision": "8819c0ec07275e5fad84",
     "url": "/static/css/main.956fad23.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.019f9528.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e967eb79f5813e467632",
-    "url": "/static/js/main.b56e0c55.chunk.js"
+    "revision": "8819c0ec07275e5fad84",
+    "url": "/static/js/main.3ef2f482.chunk.js"
   },
   {
     "revision": "f52ba179432c3be28857",
     "url": "/static/js/runtime-main.554504a0.js"
   },
   {
-    "revision": "db8ffe1de497eeb29923fa8d49a83ac3",
-    "url": "/static/media/College Resume 2020.db8ffe1d.pdf"
+    "revision": "358b296f9a8ca4d59736e9f2c2a3e6a8",
+    "url": "/static/media/College Resume 2020.358b296f.pdf"
   },
   {
     "revision": "ec9735fedae21b9edd16e88309281cda",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f82981a260cd27cc25dd03e03131ec0.js"
+  "/precache-manifest.d77d5da3ee5a63634dd680f9e2e31867.js"
 );
 
 self.addEventListener('message', (event) => {
